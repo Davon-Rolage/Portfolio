b47ccfd607daaa0f98e8fcc6cccd91b9894b49dd
Davon-Rolage <58521893+Davon-Rolage@users.noreply.github.com>
2022-11-09 03:59:11 +0300

    Add files via upload

---
+++ touch-typing/README.md
@@ -0,0 +1,3 @@
+# touch-typing
+A simple touch typing simulator that checks user input with the random generated sentence. The code uses pseudo-random module ***random***.
+Default word length is between 2 and 6 characters, default number of words in a sentence is between 3 and 10.
---
+++ touch-typing/main.py
@@ -0,0 +1,44 @@
+import random as r
+
+
+ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+# ALPHABET = ''
+
+
+def make_word():
+    global ALPHABET
+    length = r.randint(2, 6)
+    return ''.join([r.choice(ALPHABET) for _ in range(length)])
+
+
+def make_sentence():
+    amount = r.randint(3, 10)
+    sent = ' '.join([make_word() for _ in range(amount)])
+    return sent
+
+
+def check_input(user_input, sent):
+    message = 'Поздравляем! Вы красавчик.'
+
+    wrong_input = True
+    while wrong_input:
+        if user_input == sent:
+            print(message)
+            wrong_input = False
+        else:
+            print(sent)
+            message = 'Попробуй еще раз:\n'
+            user_input = input(message)
+
+
+if __name__ == '__main__':
+    wish = True
+    while wish:
+        sentence = make_sentence()
+        print(sentence)
+        user_sent = input('Повторите предложение:\n')
+        check_input(user_sent, sentence)
+
+        wish = input('Хотите ещё? да\\нет\n')
+        wish = True if wish == 'да' else False
+
